44104242724b0eb0dc73668af20748501395608a
Nicola Salsotto <nicola.salsotto@studenti.unitn.it>
2020-11-24 15:53:34 +0100

    Working login page

--- views/pages/js/userpage.js
+++ views/pages/js/userpage.js
@@ -2,3 +2,16 @@ function logout() {
     document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
     window.location.href = "/";
 }
+
+function auth(email, password) {
+    //Chiama /api/login con username e password
+    $.post('/api/login', {
+           email: email,
+           password: password
+    }, function(data) {
+        window.location.href = "/"; //Auth successful, redirect to homepage
+    })
+    .fail(function(response) {
+        alert('Errore: ' + JSON.parse(response.responseText).error);
+    });
+}
