dd1df407e9eaef79222ed161d423ddc6b77d8210
stefanichristian <christian.stefani@studenti.unitn.it>
2020-11-24 16:55:42 +0100

    aggiunta commenti in script-libro e messa in funzione di tutto libro.html, link al bottone aggiungi libro

--- views/pages/script-libro.js
+++ views/pages/script-libro.js
@@ -1,23 +1,26 @@
-var titleBook = '';
-var nickName = '';
 
-function loadUser(data) {
-  var text = '';
-  if(data[0]!==undefined) {
-    nickName=data[0].name+' '+data[0].surname;
+
+function loadUser(data) {         //load name user on the reviews
+  var nickName = '';
+  if(data!==undefined) {
+    nickName=data.name+' '+data.surname;
+    var ul = document.getElementsByName('td'+data.id);      //get the reviews that an user has written
+    for (var i = 0; i < ul.length; i++) {
+        ul[i].innerHTML = nickName;                 //set the name
+    }
   }
   else {
     nickName='ERROR';
   }
 }
 function loadReviews(data) {
-  const ul = document.getElementById('tbody');
+  const ul = document.getElementById('tbody');      //set up the page with the reviews
   var txt = '';
   if(data[0] !== undefined) {
     for(let rew of data) {
       //httpGetUser(rew.user);
       txt+= '<tr>';
-      txt+= '<td>'+rew.user+'</td>';
+      txt+= '<td name= td'+rew.user+'>'+rew.user+'</td>'; httpGetUserName(rew.user);        // now we stamp the id of user we have a p[roblem to syncronize the fun that take the name of user
       txt+= '<td>';
       for(var i=0;i<rew.rating;i++) {
         txt+= '<i class="fas fa-star"></i>' //stampo le stelline
@@ -26,9 +29,6 @@ function loadReviews(data) {
       txt+= '<td>'+rew.description+'</td>';
       txt+= '</tr>';
     }
-  //  for($i=0; $i<(int)$row['voto']; $i++)   //stampo le stelline
-      //  echo '<i class="fas fa-star"></i>';
-     // text html that we insert
   }
   else {
     txt+='<tr colspan = 3>';
@@ -40,9 +40,7 @@ function loadReviews(data) {
 function loadBooks(data) {
     const ul = document.getElementById('book');
     var txt = '';
-    console.log("enter");
-    console.log("title: "+data.title);
-    txt+= '<h1>'+data.title+'</h1>'; titleBook = data.title;
+    txt+= '<h1>'+data.title+'</h1>';
     txt+= '<h6> Author: '+data.author+'</h3>';
     txt+= '<h6> Genre: '+data.genre+'</h3>';
     txt+= '<h6> Year: '+data.year+'</h3>';
@@ -51,7 +49,7 @@ function loadBooks(data) {
     ul.innerHTML = txt; // text html that we insert
 }
 
-function httpGetUser (user) {
+function httpGetUserName (user) {
   console.log(user);            // commetto per vedere cosa chiedo
     fetch('../api/users/' + user.toString())
     .then((resp) => resp.json())
@@ -61,7 +59,7 @@ function httpGetUser (user) {
     })
     .catch( error => console.error(error) );
 }
-function httpGetBook (isbn) {
+function httpGetBook (isbn) {  //get the arg of the book and build the part of book present
   console.log(isbn);            // commetto per vedere cosa chiedo
     fetch('../api/books/' + isbn.toString())
     .then((resp) => resp.json())
@@ -72,9 +70,9 @@ function httpGetBook (isbn) {
     .catch( error => console.error(error) );
 }
 
-function httpGetReviews (isbn) {
+function httpGetReviews (isbn) {  // get the reviews of the book and build the table
   console.log(isbn);            // commetto per vedere cosa chiedo
-    fetch('../api/books/' + isbn.toString()+'/reviews')
+    fetch('../api/books/'+ isbn.toString()+'/reviews')
     .then((resp) => resp.json())
     .then(function(data) {
         loadReviews(data);
@@ -83,10 +81,11 @@ function httpGetReviews (isbn) {
     .catch( error => console.error(error) );
 }
 
-function buildPage () {
-  const queryString = window.location.search;
+function buildPage () { // fun that biuld page
+  const queryString = window.location.search;     //take isbn from url
   const urlParams = new URLSearchParams(queryString);
   const isbn = urlParams.get('isbn');
+  document.getElementById("addReview").href = "aggiungiRecensione.html?"+isbn;
   httpGetBook(isbn);
   httpGetReviews(isbn);
 
