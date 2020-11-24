50e38d8ae01c06b770172a4864b64fd9f284a0da
Nicola Salsotto <nicola.salsotto@studenti.unitn.it>
2020-11-24 17:22:06 +0100

    Profile page template

--- views/pages/js/userutils.js
+++ views/pages/js/userutils.js
@@ -10,12 +10,11 @@ function logout() {
 function auth(email, password) {
     //Chiama /api/login con username e password
     $.post('/api/login', {
-           email: email,
-           password: password
-    }, function(data) {
+       email: email,
+       password: password
+    }, (data) => {
         window.location.href = "/"; //Auth successful, redirect to homepage
-    })
-    .fail(function(response) {
+    }).fail((res) => {
         alert('Errore: ' + JSON.parse(response.responseText).error);
     });
 }
@@ -23,15 +22,14 @@ function auth(email, password) {
 function register(email, password, nome, cognome, telefono) {
     //Chiama /api/users con tutti i dati necessari
     $.post('/api/users', {
-           email: email,
-           password: password,
-           name: nome,
-           surname: cognome,
-           phone: telefono
-    }, function(data) {
+       email: email,
+       password: password,
+       name: nome,
+       surname: cognome,
+       phone: telefono
+    }, (data) => {
         window.location.href = "/signin"; //Registration successful, redirect to login page
-    })
-    .fail(function(response) {
+    }).fail((res) => {
         alert('Errore: ' + JSON.parse(response.responseText).error);
     });
 }
