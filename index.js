86154ad858e1ee6d4f2cb5b0b040e22948fa34c3
Carlo Ramponi <magocarlos1999@gmail.com>
2020-11-30 18:50:20 +0100

    Token authentication and MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const app = require('./lib/app.js');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 8000;
+let mongourl;
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+if(process.env.MONGOURL) {
+    mongourl = process.env.MONGOURL
+} else {
+    mongourl = require('./secrets.json').mongodb_url;
+}
+
+app.locals.db = mongoose.connect(mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
 });
+
+
